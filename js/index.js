c11f8123a4c201d7a18d311ff5220becdae18e17
Suu01 <ksj19990823@daum.net>
2022-07-05 12:52:42 +0900

    sub-menu-mobile

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 window.onload = function() {
 
-    // 이벤트를 실행시키기 위한 요소 불러들이기
+    // 이벤트를 실행시키기 위한 요소
     let brandTitle1 = document.querySelector('.brand-text1 a');
     let brandTitle2 = document.querySelector('.brand-text2 a');
     let brandTitle3 = document.querySelector('.brand-text3 a');
     let brandImage = document.querySelector('.brand-img');
+
+    // 모바일 상세메뉴 창 불러들이고 닫기 위한 요소
+    let hamBtn = document.querySelector('#ham');
+    let closeBtn = document.querySelector('.closebtn');
+    let subMenu = document.querySelector('.sub-menu');
 
 
     // brand의 title 클릭 시 bg-img 전환
@@ -23,4 +28,18 @@ window.onload = function() {
         brandImage.innerHTML = changeImg3;
         preventDefault();
     }
+
+
+    // (모바일) 햄버거버튼 클릭 시 상세메뉴 open
+    closeBtn.onclick = function() {
+        subMenu.style.width = '0%';
+    }
+
+    // (모바일) 닫기버튼 클릭 시 상세메뉴 close
+    hamBtn.onclick = function() {
+        subMenu.style.width = '100%';
+    }
+
+
+
 };
